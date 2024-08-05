9c2fcf5709064930f77c09a7a94ba6387a7a4a45
Anthony Lee <anthonylee1994@users.noreply.github.com>
2024-08-05 16:26:39 +0800

    change to textarea

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import Groq from "groq-sdk";
-import {Box, Button, Flex, Input, InputGroup, InputRightElement} from "@chakra-ui/react";
+import {Box, Button, Flex, Input, InputGroup, InputRightElement, Textarea} from "@chakra-ui/react";
 import {MarkdownPreview} from "./components/MarkdownPreview";
 
 interface Message {
@@ -47,7 +47,7 @@ export const App = () => {
 
     return (
         <Flex flexDir="column">
-            <Box flex={1} p={4} overflowY="auto" pb={20}>
+            <Box flex={1} p={4} overflowY="auto" pb={120}>
                 <Box maxW="2xl" mx="auto">
                     {messages.map((message, index) => (
                         <Box key={index} mb={2} bg="gray.200" p={4} borderRadius="md" bgColor={message.role === "user" ? "blue.100" : "gray.100"} overflow="auto">
@@ -72,7 +72,7 @@ export const App = () => {
             >
                 <form style={{display: "flex", width: "100%"}} onSubmit={handleSubmit}>
                     <InputGroup size="md">
-                        <Input pr="4.5rem" placeholder="Ask for question" value={inputValue} onChange={event => setInputValue(event.target.value)} />
+                        <Textarea mb={{base: 2, sm: 0}} pr="4.5rem" placeholder="Ask for question" value={inputValue} onChange={event => setInputValue(event.target.value)} />
                         <InputRightElement width="4.5rem">
                             <Button h="1.75rem" size="sm" type="submit" isLoading={isLoading}>
                                 Send
